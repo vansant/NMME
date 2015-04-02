5ad45e70bb0f263c6de2ddcb3556a6e05cb2e28c
ubuntu <ubuntu@ubuntu.(none)>
2015-04-02 16:20:13 -0700

    Added day querystring parameter an error checking

--- NMME/views.py
+++ NMME/views.py
@@ -6,5 +6,20 @@ def index(request):
     return HttpResponse("Hello, world")
 
 def get_netcdf_data(request):
-    netcdf_data = models.get_data(day=1, lat=42.68244, lon=-113, positive_east_longitude=True, variable='specific_humidity')    
+    errors = []
+
+    if 'day' in request.GET:
+        try:
+        	day = int(request.GET['day'])
+        except:
+        	errors.append("Day parameter needs to be an integer")
+    else:
+    	errors.append("You need to specify a day parameter")
+
+    print errors
+
+    if errors:
+    	return HttpResponse(errors)
+    else:
+        netcdf_data = models.get_data(day=day, lat=42.68244, lon=-113, positive_east_longitude=True, variable='specific_humidity')    
     return HttpResponse(netcdf_data)
