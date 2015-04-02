72b53e48e3f3f137bb1eb649cf762310109cffd7
ubuntu <ubuntu@ubuntu.(none)>
2015-04-02 15:37:35 -0700

    Updated URLs.py to separate Production /Services urls from development / URLs

--- NMME/urls.py
+++ NMME/urls.py
@@ -3,11 +3,26 @@ from django.contrib import admin
 
 import views
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'NMME.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    #url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'NMME.views.index', name='index'),
-    url(r'^get-netcdf-data/', 'NMME.views.get_netcdf_data', name='get_netcdf_data'),
+from local_settings.py import DEBUG
+
+if debug == True:
+    urlpatterns = patterns('',
+        # Examples:
+        # url(r'^$', 'NMME.views.home', name='home'),
+        # url(r'^blog/', include('blog.urls')),
+        #url(r'^admin/', include(admin.site.urls)),
+        url(r'^$', 'NMME.views.index', name='index'),
+        url(r'^get-netcdf-data/', 'NMME.views.get_netcdf_data', name='get_netcdf_data'),
 )
+
+# URLs to work with sub path in Production /Services
+else:
+    urlpatterns = patterns('',
+        # Examples:
+        # url(r'^$', 'NMME.views.home', name='home'),
+        # url(r'^blog/', include('blog.urls')),
+        #url(r'^admin/', include(admin.site.urls)),
+        url(r'^$', 'NMME.views.index', name='index'),
+        url(r'^/get-netcdf-data/', 'NMME.views.get_netcdf_data', name='get_netcd$
+)
+
