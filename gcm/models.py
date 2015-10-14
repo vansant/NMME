e7f09f480eaa75c46e32e3167f1aad288539efe5
vansant <donny.vansant@gmail.com>
2015-10-14 15:57:19 -0700

    Get Scatterplot Data
    Working example of getting spatial (netcdf subset) and temporal (DJF 3
    month) average from a NetCDF4 dataset

--- gcm/models.py
+++ gcm/models.py
@@ -1,3 +1,74 @@
-from django.db import models
+import math
+import numpy as np
+import pandas as pd
 
-# Create your models here.
+def get_date_no_leap_year(index):
+    """ Function to calculate the year-month-day from netCDF4 days since 1900-01-01 index which are calculated with no leap years"""
+    year = 1900 + math.floor(index/365)
+    doy = index - math.floor(index/365)*365
+    month_sum_array = np.array([31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365])
+    if doy < month_sum_array[0]:
+    	day = doy - 1
+    	month = 0
+    else:
+    	month = np.abs(month_sum_array - doy).argmin()
+    	day = doy - month_sum_array[month] - 1
+        month = month + 1
+    return "%d-%02d-%d" % (year, month+1, day)
+
+def spatial_subset(data, method):
+	""" Returns the spatial average or sum for a NetCDF subset region"""
+	if method == "sum":
+	    return [np.sum(x) for x in data]
+	elif method == "mean":
+		return [np.mean(x) for x in data]
+
+def custom_resampler(array):
+    """ Pandas custom resampler to include resampled dates only if they are available"""
+    # print len(array), custom_span_global
+
+    if len(array) == custom_span_global:
+        if sample_method_global == "mean":
+            return np.mean(array)
+        elif sample_method_global == "sum":
+            return np.sum(array)
+
+def process_gcm_data(custom_span, sample_method, date_list, data, start_year, end_year, start_month, end_month):
+
+    # Global variables set to be called from custom_resampler method
+    global custom_span_global
+    global sample_method_global
+    custom_span_global = custom_span
+    sample_method_global = sample_method
+
+    #print date_list[-1], "This is the index"
+    #print len(date_list), "this is where it is passed in"
+
+    # Convert list of dates to pandas date_range 
+    pandas_date_range = pd.DatetimeIndex(date_list)
+
+    # pandas_date_range = pd.date_range(start="1950-01", end="2005-12", freq="12M")
+    # print pandas_date_range, "This si what im looking for"
+    # #print len(pandas_date_range), "What epandas expects"
+
+    # Create pangdas time series 
+    time_series = pd.Series(data, index=pandas_date_range)
+    #print time_series
+    #print len(time_series)
+
+    # # Resample daily to monthly values
+    # month_resample = time_series.resample('M', how=sample_method)
+    # #print month_resample
+
+    # Filter out the dates of concern
+    #print (start_year,start_month), "this is satataetated"
+    filtered_time_range =  time_series['%s-%s'%(start_year,start_month):'%s-%s'%(end_year,end_month)]
+    print filtered_time_range
+
+    # M is for pandas months here
+    time_span = '%sM' % custom_span
+
+    # Resample daily to monthly values
+    resampled_time_series = filtered_time_range.resample(time_span, how=custom_resampler, closed="left", label="right")
+    #print resampled_time_series
+    return resampled_time_series
--- gcm/views.py
+++ gcm/views.py
@@ -1,3 +1,134 @@
-from django.shortcuts import render
+import numpy as np
 
-# Create your views here.
+from django.http import HttpResponse
+from netCDF4 import Dataset
+
+from .models import  get_date_no_leap_year, spatial_subset, custom_resampler, process_gcm_data
+
+def get_scatterplot_data(request):
+	""" View that calculate the average DJF from a NETCDF4 file for a spatial region"""
+
+	# URL parameters
+	#data_path = "http://thredds.northwestknowledge.net:8080/thredds/dodsC/NWCSC_INTEGRATED_SCENARIOS_ALL_CLIMATE/projections/nmme/bcsd_nmme_metdata_NCAR_forecast_daily.nc"
+	data_path = "http://thredds.northwestknowledge.net:8080/thredds/dodsC/macav2livneh_pr_BNU-ESM_r1i1p1_historical_1950_2005_CONUS_monthly_aggregated.nc"
+	variable = "precipitation"
+	request_dates = "True"
+	positive_east_longitude = False
+	lat = 45
+	lon = -110
+	request_lat_lon = "False"
+
+	# File handles
+	pathname = data_path
+	filehandle=Dataset(pathname,'r',format="NETCDF4")
+	#print filehandle.variables
+	try:
+	    lathandle = filehandle.variables['lat']
+	except:
+	    lathandle = filehandle.variables['Latitude']
+	try:
+	    lonhandle=filehandle.variables['lon']
+	except:
+	    lonhandle=filehandle.variables['Longitude']
+	try:
+	    timehandle=filehandle.variables['time']
+	except:
+	    try:
+	        timehandle=filehandle.variables['Time']
+	    except:
+	        timehandle=filehandle.variables['day']
+
+	# NetCDF variable 
+	datahandle=filehandle.variables[variable]
+	time_num=len(timehandle)
+	timeindex=range(time_num)  #python starts arrays at 0
+	time=timehandle[timeindex]
+
+	# Get lat slice range
+	# Need to pass in index of the lat, lon bounding box here
+
+	# Bounding box indices
+	sw_lat = ""
+	sw_lon = ""
+	ne_lat = ""
+	ne_lon = ""
+
+	closestLat = slice(200, 210)
+	closestLon = slice(200, 210)
+
+	# Which months to get data for
+	month_list = [11,0,1]
+
+	def get_monthly_dates_and_data(month):
+		""" Function that get dates and data for a single month from a NetCDF File"""
+		# Month to get data for
+		request_month = month
+
+		print request_month
+
+		# Dimensions
+		variable_dimensions = datahandle.dimensions
+
+		# Dictionary to map dimension with index value
+		variable_index_dictionary = {}
+
+		# Slice out month data for all years
+		timeindex = slice(request_month,len(timehandle),12)
+
+		#print timeindex
+		for var in variable_dimensions:
+		    if var == "time" or var == "Time" or var == "day":
+		        variable_index_dictionary[var] = timeindex
+		    if var == "lat" or var == "Latitude":
+		        variable_index_dictionary[var] = closestLat
+		    if var == "lon" or var == "Longitude":
+		        variable_index_dictionary[var] = closestLon
+
+		# Dictionary to map the order of the dimensions
+		variable_dimensions_dictionary = {}
+		variable_dimensions_dictionary[0] = datahandle.dimensions[0]
+		variable_dimensions_dictionary[1] = datahandle.dimensions[1]
+		variable_dimensions_dictionary[2] = datahandle.dimensions[2]
+		#print variable_dimensions_dictionary
+
+		# Get the dates using calculation for now leap years
+		dates = [get_date_no_leap_year(x) for x in timehandle[timeindex]]
+		#print dates, len(dates)
+
+		# Get the data
+		data = datahandle[variable_index_dictionary[variable_dimensions_dictionary[0]], variable_index_dictionary[variable_dimensions_dictionary[1]], variable_index_dictionary[variable_dimensions_dictionary[2]]]
+		#print  "Temporal resolution from NetCDF= ", len(data[:])
+
+		# Average or sum over spatial subset region
+		data_spatial_analysis = spatial_subset(data, method="mean")
+		#print "Spatial subset temporal resolution = ", len(data_spatial_analysis)
+
+		dataset = []
+		for i in range(len(dates)):
+			dataset.append([dates[i], data_spatial_analysis[i]])
+		return dataset
+
+	# Get and sort monthly dates and data
+	monthly_dates_and_data = []
+	for x in month_list:
+		monthly_dates_and_data += get_monthly_dates_and_data(x)
+	monthly_dates_and_data.sort()
+
+	print monthly_dates_and_data
+
+	date_list = [i[0] for i in monthly_dates_and_data]
+	data = [i[1] for i in monthly_dates_and_data]
+	#print date_list
+
+	results = process_gcm_data(custom_span=len(month_list), sample_method="mean", date_list=date_list, data=data, start_year=1950, end_year=2100, start_month=month_list[0]+1, end_month=month_list[-1]+1)
+
+	# Drop nans
+	results = results.dropna()
+	print results, len(results), "results",
+	print "Average for all specified years: ", np.mean(results)
+	
+	return HttpResponse(np.mean(results))
+
+
+# 35.909229 + 23.590954 + 38.585739
+
